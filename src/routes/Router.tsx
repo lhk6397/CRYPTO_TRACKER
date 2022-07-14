fe0f7ca3b4879d78f9524cb37a0c3f37611af34a
maruhxn <a23686397@gmail.com>
2022-07-14 15:04:47 +0900

    Modification

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chart from "./Chart";
 import Coin from "./Coin";
 import Coins from "./Coins";
 import Price from "./Price";
 
-interface IRouterProps {}
-
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />}></Route>
         <Route path="/:coinId" element={<Coin />}>
